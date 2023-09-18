400de5046da0fb97e56e15cf354b5957978471bb
RyannKim327 <mpoprev2@email.com>
2023-09-18 17:24:29 +0800

    9/18/2023 5:24:28 PM [Auto Commit]

--- script/help.js
+++ script/help.js
@@ -2,8 +2,8 @@ const fs = require("fs")
 
 module.exports = async (bot, msg, match)  => {
 	const json = JSON.parse(fs.readFileSync("config.json", "utf-8"))
-	let _msg = `Good day Mr/Ms. @Username, I am "${json.name}". Here are the commands you may use for my service\n\n`
-	for(let c = c < json.commands.length; c++){
+	let _msg = `Good day Mr/Ms. @${msg.chat.username}, I am "${json.name}". Here are the commands you may use for my service\n\n`
+	for(let c = 0; c < json.commands.length; c++){
 		let cmd = json.commands[c]
 		_msg += `${cmd.title}\n- ${cmd.description}\nCommand: ${json.prefix}${cmd.cmd}`
 		if(c < json.commands.length - 1){

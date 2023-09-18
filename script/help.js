6c3f6cf85711efae5cf5ad5368f78d85d489082d
RyannKim327 <mpoprev2@email.com>
2023-09-18 17:15:44 +0800

    9/18/2023 5:15:43 PM [Auto Commit]

--- script/help.js
+++ script/help.js
@@ -2,6 +2,6 @@ const fs = require("fs")
 
 module.exports = async (bot, msg, match)  => {
 	const json = JSON.parse(fs.readFileSync("config.json", "utf-8"))
-	let msg = `Good day Mr/Ms. @${msg.chat.username}`
+	let msg = `Good day Mr/Ms. @${msg.chat.username}, I am "${json.name}". Here are my commands`
 	bot.sendMessage(msg.chat.id, "Test Help")
 }

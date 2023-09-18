68dd8409e363106640f1a768e299b65c88cd3df5
RyannKim327 <mpoprev2@email.com>
2023-09-18 17:09:09 +0800

    9/18/2023 5:09:09 PM [Auto Commit]

--- script/help.js
+++ script/help.js
@@ -1,6 +1,7 @@
 const fs = require("fs")
 
 module.exports = async (bot, msg, match)  => {
-	const json = fs.re
+	const json = JSON.parse(fs.readFileSync("./../config"))
+	
 	bot.sendMessage(msg.chat.id, "Test Help")
 }

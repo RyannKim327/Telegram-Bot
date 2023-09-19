48a45bb3e1cbeb6ebd218b1e47e789e10d5507a5
RyannKim327 <mpoprev2@email.com>
2023-09-19 20:52:01 +0800

    9/19/2023 8:52:01 PM [Auto Commit]

--- script/help.js
+++ script/help.js
@@ -5,14 +5,16 @@ module.exports = async (bot, msg, match, isReply)  => {
 	let _msg = `Good day Mr/Ms. [@${msg.from.username}](tg://user?id=${msg.from.id}), I am "${json.name}". Here are the commands you may use for my service\n\n`
 	for(let c = 0; c < json.commands.length; c++){
 		let cmd = json.commands[c]
-		if(cmd != undefined){
+		if(.admin != undefined){
 			let tut = cmd.tut || cmd.cmd.replace("(.*)", "‹‹ query ››")
 			_msg += `${cmd.title}\n- ${cmd.description}\nCommand: ${json.prefix}${tut}`
 			if(c < json.commands.length - 1){
 				_msg += "\n"
 			}
 			_msg += "\n"
-		}else if(cmd.admin)
+		}else if(!cmd.admin){
+
+		}
 	}
 	bot.sendMessage(msg.chat.id, _msg, {
 		// "reply_to_message_id": msg.message_id,

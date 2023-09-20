d2f6f7a1eb590e33fc10383ba1a7a6e4e3cb2e9b
RyannKim327 <mpoprev2@email.com>
2023-09-20 21:33:59 +0800

    9/20/2023 9:33:59 PM [Auto Commit]

--- admin/subscribe.js
+++ admin/subscribe.js
@@ -1,5 +1,5 @@
 const fs = require("fs")
 module.exports = (bot, msg, match) => {
 	const json = JSON.parse(fs.readFileSync("data.json", "utf-8"))
-	if(json.subs)
+	if(json.subscribe.includes(bot.msg.id))
 }

4e2935b8343015bf4a38be917835c8ec46e06c76
RyannKim327 <mpoprev2@email.com>
2023-09-20 21:37:01 +0800

    9/20/2023 9:37:01 PM [Auto Commit]

--- admin/subscribe.js
+++ admin/subscribe.js
@@ -1,10 +1,10 @@
 const fs = require("fs")
 module.exports = (bot, msg, match) => {
 	const json = JSON.parse(fs.readFileSync("data.json", "utf-8"))
-	if(json.subscribe.includes(bot.chat.message_thread_id)){
+	if(json.subscribe.includes(bot.message_thread_id)){
 		bot.sendMessage(msg.chat.id, "This thread is already added.")
 	}else{
-		json.subscribe += bot.chat.message_thread_id
+		json.subscribe += bot.message_thread_id + ", "
 		bot.sendMessage(msg.chat.id, "This thread is now subscribed to a special feature.")
 	}
 }
